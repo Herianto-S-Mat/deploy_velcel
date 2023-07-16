331121694b57536b88642a3c16de765314801e12
Herianto <herianto9671@gmail.com>
2023-07-16 18:44:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const hapi = require('@hapi/hapi');
+
+const server = hapi.server({
+    port: 3000,
+    host: '0.0.0.0',
+});
+
+
+server.route([
+    {
+    method: 'GET',
+    path: '/',
+    handler: async(h) =>{
+        const response = await h.response({
+            "massage" :"cscs",
+        });
+        await response.code(200);
+        return response
+    },
+    }
+]);
+
+server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
